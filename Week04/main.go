9cc0b15d76e2f38685cc6062787f4c210369cbd2
binz <876507628@qq.com>
2020-12-16 23:32:46 +0800

    week04 作业 wire初始化资源

--- Week04/main.go
+++ Week04/main.go
@@ -10,5 +10,11 @@ func main() {
 		"代码需要包含对数据层、业务层、API 注册，以及 main 函数对于服务的注册和启动，信号处理，",
 		"使用 Wire 构建依赖。可以使用自己熟悉的框架")
 
+}
+
+func init() {
+	fmt.Println("使用wire初始化server等资源")
+	fmt.Println(" wire.Build(Server, Signal)")
 	server.StartServer()
+
 }
