2ac6feb95789bd6428ec61f7d21e36e6f3694f99
binz <876507628@qq.com>
2021-01-27 23:10:19 +0800

    go tcp server

--- Week09/main.go
+++ Week09/main.go
@@ -14,7 +14,6 @@ import (
 func main() {
 	fmt.Println("1. 用 Go 实现一个 tcp server，",
 		"用两个 goroutine 读写 conn，两个 goroutine 通过 chan 可以传递 message，能够正确退出")
-	fmt.Println("https://github.com/Go-000/Go-000/issues/82")
 	tcpAddr := net.TCPAddr{
 		IP:   []byte{127, 0, 0, 1},
 		Port: 6666,
